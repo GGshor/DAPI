1eed883629cad681e8616cc1526a73d3fdf8d85b
GGshor <73340248+GGshor@users.noreply.github.com>
2022-09-08 21:54:37 +0200

    Added CheckArgumentCharacters

--- src/Utils.lua
+++ src/Utils.lua
@@ -1,6 +1,7 @@
 local Utils = {}
 
-function Utils.CheckArgument(types: {string}, ...: any)
+
+function Utils.CheckArgumentTypes(types: {string}, ...: any)
     for index, argument in ipairs({...}) do
         if typeof(argument) ~= types[index] then
             if types[index]:find("?") and typeof(argument) == "nil" then
@@ -10,5 +11,14 @@ function Utils.CheckArgument(types: {string}, ...: any)
         end
     end
 end
+
+function Utils.CheckArgumentCharacters(limits: {number}, ...:string)
+    for index, argument in ipairs({...}) do
+       if argument:len() > limits[index] then
+        error("Expected " .. tostring(limits[index]) .. "characters or less, got " .. tostring(argument:len()))
+       end
+    end
+end
+
 
 return Utils
