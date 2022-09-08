1e21080bef22ccd0a3e85b561b757568b02fd031
GGshor <73340248+GGshor@users.noreply.github.com>
2022-09-08 21:14:55 +0200

    Added Embed character counter

--- src/Embed.lua
+++ src/Embed.lua
@@ -41,10 +41,52 @@ export type EmbedAuthor = {
 export type EmbedField = {
 	name: string,
 	value: string,
-	inline: boolean?
+	inline: boolean
 }
 
+local Utils = require(script.Parent.Utils)
 
 local Embed = {}
+Embed.__index = Embed
+
+
+function Embed:AddField(name: string, value: string, inline: boolean?)
+	Utils.CheckArgument({"string", "value", "boolean?"}, name, value, inline)
+end
+
+function Embed:CountCharacters()
+	local result = {total=0}
+
+	if self.title then
+		result.title = self.title:len()
+		result.total += self.title:len()
+	end
+
+	if self.description then
+		result.description = self.description:len()
+		result.total += self.description:len()
+	end
+
+	for field: EmbedField in self.fields do
+		result.fields[field.name] = field.value:len() + field.name:len()
+		result.total += field.value:len() + field.name:len()
+	end
+
+	if self.footer then
+		result.footer = self.footer:len()
+		result.total += self.footer:len()
+	end
+
+	if self.author then
+		result.author = self.author.name:len()
+		result.total += self.author.name:len()
+	end
+
+	return result.total, result
+end
+
+function Embed.new()
+	
+end
 
 return Embed
