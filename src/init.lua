67febca6c604c0d00072187b139a34d189fc80f7
GGshor <73340248+GGshor@users.noreply.github.com>
2022-10-29 08:37:40 +0200

    Finished main module

--- src/init.lua
+++ src/init.lua
@@ -8,13 +8,22 @@ local Embed = require(script.Embed)
 local Utils = require(script.Utils)
 local Webhook = require(script.Webhook)
 
---[[
-    Making it easier to send message through webhooks.
+--[=[
+    Making it easier to send messages through webhooks.
 
     @class RoCord
-]]
+]=]
 local RoCord = {}
 
+--[=[
+	Adds an embed to the message and returns the embed so that you can change it.
+
+	@param title string? -- Title of the embed
+	@param description string? -- Description of the embed
+	@param color Color3? -- Color of the embed
+
+	@return Embed
+]=]
 function RoCord:AddEmbed(title: string?, description: string?, color: Color3?)
 	local embed = Embed.new(title, description, color)
 
@@ -23,18 +32,58 @@ function RoCord:AddEmbed(title: string?, description: string?, color: Color3?)
 	return embed
 end
 
-function RoCord:SendMessage(content: string, username: string?)
-	Utils.CheckArgumentCharacters({ Utils.Limits.Content}, content)
-	self.webhook:Execute()
+--[=[
+	Sends the message with added embeds.
+	If the message was send succesfully then body is cleared back to default.
+
+	@param content string? -- The text of the message
+	@param username string? -- Override the username of webhook
+]=]
+function RoCord:SendMessage(content: string?, username: string?)
+	if content then
+		Utils.CheckArgumentTypes({ "string" }, content)
+		Utils.CheckArgumentCharacters({ Utils.Limits.Content }, content)
+
+		self.body.content = content
+	end
+
+	if username then
+		Utils.CheckArgumentTypes({ "string" }, username)
+		Utils.CheckArgumentCharacters({ Utils.Limits.UserName }, username)
+
+		if username:len() < 2 then
+			error("Expected username to have more than 2 characters, got " .. tostring(username:len()))
+		end
+
+		self.body.username = username
+	end
+
+	self.webhook:Execute(self.body)
+
+	self.body = {
+		content = "",
+		username = self.webhook.name,
+		tts = false,
+		embeds = {}
+	}
 end
 
-function RoCord:ChangeWebhook(webhookUrl: string)
-	self.webhook:Update(webhookUrl)
+--[=[
+	Updates the webhook url.
+
+	@param webhookURL string -- The new webhook url
+]=]
+function RoCord:ChangeWebhook(webhookURL: string)
+	self.webhook:Update(webhookURL)
 end
 
---[[
-	Inits a new setup
-]]
+--[=[
+	Creates a new webhook setup.
+
+	@param webhookURL string -- The webhook url, can be a proxy but needs to support get and post requests
+
+	@return RoCord
+]=]
 function RoCord.new(webhookURL: string)
 	local self = setmetatable({}, RoCord)
 
